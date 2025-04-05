7546956f7b413bc8b089af35377f7b97d3d721f2
Alireza <azerila406@gmail.com>
2025-04-05 22:17:42 +0330

    add more AuthErrors

--- src/models/result/errorTypes/AuthError.java
+++ src/models/result/errorTypes/AuthError.java
@@ -1,9 +1,12 @@
 package models.result.errorTypes;
 
 public enum AuthError implements Error {
-    INVALID_CREDENTIALS("Invalid credentials"),
+    INVALID_CREDENTIALS("Invalid username or password"),
     USER_NOT_FOUND("User not found"),
-    USER_ALREADY_EXISTS("User already exists");
+    USER_ALREADY_EXISTS("User already exists"),
+    PASSWORD_TOO_WEAK("Password does not meet security requirements"),
+    EMAIL_ALREADY_REGISTERED("Email is already registered"),
+    INVALID_EMAIL_FORMAT("Invalid email format");
 
     private final String message;
 
