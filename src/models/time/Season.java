ced0613f04a582ce635ec0aaf734feb4aa00ade8
Alireza <azerila406@gmail.com>
2025-04-06 00:50:19 +0330

    add simple date

---
+++ src/models/time/Season.java
@@ -0,0 +1,12 @@
+package models.time;
+
+public enum Season {
+    SPRING,
+    SUMMER,
+    AUTUMN,
+    WINTER;
+
+    public Season nextSeason() {
+        return values()[(this.ordinal() + 1) % values().length];
+    }
+}
