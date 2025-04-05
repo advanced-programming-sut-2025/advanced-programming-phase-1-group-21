d28830619c5bf74a3c9d030d77258c3adc820ac8
Alireza <azerila406@gmail.com>
2025-04-06 00:23:55 +0330

    add user changes and user info

--- src/controllers/UserController.java
+++ src/controllers/UserController.java
@@ -26,4 +26,24 @@ public class UserController {
     public Result<Void> saveUser() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    public Result<Void> changeUsername(String currentUsername, String newUsername) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Result<Void> changePassword(String username, String oldPassword, String newPassword) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Result<Void> changeEmail(String username, String newEmail) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Result<Void> changeNickname(String username, String newNickname) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Result<String> showUserInfo(String username) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
