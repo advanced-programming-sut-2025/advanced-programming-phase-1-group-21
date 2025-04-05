6df5d7b6dc7c7894b836c1b8aa4c246d7a854117
Alireza <azerila406@gmail.com>
2025-04-05 23:40:07 +0330

    Add user

---
+++ src/controllers/UserController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+public class UserController {
+
+    private User user;
+
+    public UserController(User user) {
+        this.user = user;
+    }
+
+    public static boolean isUsernameValid(String username) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public static boolean isPasswordValid(String password) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public static boolean isEmailValid(String email) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
