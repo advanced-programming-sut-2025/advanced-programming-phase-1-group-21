00e14ce2fa663878a372fec7cbdfeac674f04476
Alireza <azerila406@gmail.com>
2025-04-06 00:38:37 +0330

    add basic game system

---
+++ src/controllers/GameController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import java.util.List;
+
+public class GameController {
+
+
+    public Result<Game> createGame(List<User> users) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ src/models/game/Player.java
@@ -0,0 +1,9 @@
+package models.game;
+
+import models.user.User;
+
+public class Player {
+    private User user;
+    private Map map;
+    private Game game;
+}
