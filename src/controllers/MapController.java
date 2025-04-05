0f932d4c60c480541686973e765ef58d42fda5c2
Alireza <azerila406@gmail.com>
2025-04-06 01:06:17 +0330

    add simple map

---
+++ src/controllers/MapController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+public class MapController {
+    private Map map;
+
+    public MapController() {
+        this.map = new Map();
+    }
+   
+    public void printMap(Cord cord, int radius) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void helpReadingMap() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
--- src/models/map/Map.java
+++ src/models/map/Map.java
@@ -2,4 +2,8 @@ package models.map;
 
 public class Map {
     private int id;
+
+    public int calculateDistance(Cord start, Cord end) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
