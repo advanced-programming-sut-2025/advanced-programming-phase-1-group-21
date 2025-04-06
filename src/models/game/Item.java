eff2e1c8ffcff772133db89738244a326f7d75e2
Alireza <azerila406@gmail.com>
2025-04-06 14:45:20 +0330

    fix bug

--- src/models/game/Item.java
+++ src/models/game/Item.java
@@ -3,5 +3,17 @@ package models.game;
 public class Item {
     String name;
     ItemType itemType;
-    
+
+    public Item(String name, ItemType itemType) {
+        this.name = name;
+        this.itemType = itemType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ItemType getItemType() {
+        return itemType;
+    }
 }
