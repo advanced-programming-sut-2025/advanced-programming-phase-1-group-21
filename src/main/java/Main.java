a38b8d1910373d874fa8b62f301be42a51488ce2
ParsaAp0 <parsaadlparvar.201.1384@gmail.com>
2025-04-25 17:04:04 +0330

    Still trying

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 import views.menu.AppView;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         AppView appView = new AppView();
-        appView.run();
+        Scanner scanner = new Scanner(System.in);
+        String test = scanner.next();
+        System.out.println("this is test: " + test);
+//        appView.run();
     }
 
 }
