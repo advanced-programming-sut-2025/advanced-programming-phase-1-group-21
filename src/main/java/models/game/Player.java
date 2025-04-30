98e32250bd417cb8c530df00239cd9f0ba9b437e
Alireza <azerila406@gmail.com>
2025-04-30 16:46:28 +0330

    Add energy

--- src/main/java/models/game/Player.java
+++ src/main/java/models/game/Player.java
@@ -8,13 +8,30 @@ import java.util.ArrayList;
 public class Player {
     private User user;
     private Map map;
-    private Energy energy;
+    private int currentEnergy;
+    private int maxEnergy;
     private Inventory inventory;
     private TrashCanType trashCanType;
     private int coins;
     private Item itemInHand;
     private ArrayList<Relation> relations;
     private ArrayList<NPCFriendship> npcFriendships;
+
+    public int getCurrentEnergy() {
+        return currentEnergy;
+    }
+
+    public int getMaxEnergy() {
+        return maxEnergy;
+    }
+
+    public void decreaseEnergy(int amount) {
+        if (currentEnergy < amount) {
+            throw new IllegalArgumentException("You can't decrease energy by " + amount);
+        }
+        currentEnergy -= amount;
+    }
+
 
 
     public void addCoins(int coins) {
