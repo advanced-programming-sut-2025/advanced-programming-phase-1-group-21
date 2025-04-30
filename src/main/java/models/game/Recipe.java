639ad0eb1cdc1db54660eccd6c12a5d1b73ea444
Alireza <azerila406@gmail.com>
2025-04-30 20:05:20 +0330

    recipe toString

--- src/main/java/models/game/Recipe.java
+++ src/main/java/models/game/Recipe.java
@@ -10,4 +10,28 @@ public class Recipe {
         this.items = items;
         this.result = result;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < items.size(); i++) {
+            Item item = items.get(i);
+            builder.append(item.toString());
+            if (item.getAmount() > 1) {
+                builder.append(" * ").append(item.getAmount());
+            }
+
+            if (i < items.size() - 1) {
+                builder.append(" + ");
+            }
+        }
+
+        builder.append(" = ").append(result.toString());
+        if (result.getAmount() > 1) {
+            builder.append(" * ").append(result.getAmount());
+        }
+
+        return builder.toString();
+    }
 }
