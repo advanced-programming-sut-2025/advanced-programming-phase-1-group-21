c9d6d75ff53ed1fba093a6593324b71ee027809f
Alireza <azerila406@gmail.com>
2025-04-30 19:50:34 +0330

    small bug

--- src/main/java/models/game/Game.java
+++ src/main/java/models/game/Game.java
@@ -18,7 +18,7 @@ public class Game {
     private Weather gameWeather;
     private Weather forecastCheat;
 
-    public Game(List<Player> players) {
+    public Game(ArrayList<Player> players) {
         this.players = players;
         this.currentPlayer = players.get(0);
         this.gameDate = Date.createBias();
