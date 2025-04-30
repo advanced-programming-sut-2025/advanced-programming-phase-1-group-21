1a6d3a8fc87f64f8347a2414da6e5f0cf2166b86
sobhan <sobhan.math.1384@gmail.com>
2025-04-30 22:11:45 +0330

    finish walk

--- src/main/java/models/map/GreenHouse.java
+++ src/main/java/models/map/GreenHouse.java
@@ -1,5 +1,20 @@
 package models.map;
 
+import java.util.ArrayList;
+
 public class GreenHouse extends Building {
+
+    public GreenHouse() {
+        this.tiles = new ArrayList<>();
+        for(int i = 0 ; i < 10 ; i++){
+            ArrayList<Tile> thisRowTiles = new ArrayList<>();
+            for(int j = 0 ; j < 20 ; j++){
+                thisRowTiles.add(new Tile(new Coord(j , i) , null , false , false ,
+                        false , false , null , null));
+            }
+            tiles.add(thisRowTiles);
+        }
+        this.tiles.get(9).get(19).setDoor(true);
+    }
 
 }
