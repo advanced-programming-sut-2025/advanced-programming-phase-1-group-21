e0c663a198e5ab81ae4c4c305255f2c29ea7455d
Alireza <azerila406@gmail.com>
2025-05-03 10:22:29 +0330

    refactor

--- src/main/java/models/map/Coord.java
+++ src/main/java/models/map/Coord.java
@@ -1,5 +1,10 @@
 package models.map;
 
+import models.App;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coord {
     private int x;
     private int y;
@@ -15,5 +20,22 @@ public class Coord {
 
     public int getY() {
         return y;
+    }
+
+    public static Coord getValidCoord(int x, int y) {
+        Coord coord = new Coord(x, y);
+        if (coord.getX() < 0 || coord.getY() < 0) return null;
+        ArrayList<ArrayList<Tile>> tiles = App.game.getCurrentPlayer().currentLocationTiles();
+        if(y >= tiles.size() || x >= tiles.get(0).size())
+            return null;
+        return coord;
+    }
+
+    public Coord getValidCoord() {
+        return getValidCoord(x, y);
+    }
+
+    public Coord addCoord(Coord coord) {
+        return new Coord(x + coord.getX(), y + coord.getY());
     }
 }
