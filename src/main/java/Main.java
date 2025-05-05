35a923f21f7d519b6456936aa44a2f37eb5d11a8
ParsaAp0 <parsaadlparvar.201.1384@gmail.com>
2025-05-05 15:15:48 +0330

    Remove extra changes. now "Main.java" is back to normal.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,3 @@
-import models.data.AnimalHouseData;
-import models.data.DataLoader;
-import models.data.StardropSaloonRecipeData;
 import views.menu.AppView;
 
 import java.io.IOException;
@@ -8,8 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         AppView appView = new AppView();
-//        appView.run();
-        DataLoader.load();
+        appView.run();
     }
 
 }
