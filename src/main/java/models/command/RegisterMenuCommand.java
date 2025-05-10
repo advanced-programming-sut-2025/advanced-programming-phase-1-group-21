2e8a402812905cb44a4e0b1a902662ef6f3f3c11
Alireza <azerila406@gmail.com>
2025-05-11 02:10:15 +0330

    enum name changed

--- src/main/java/models/command/RegisterMenuCommand.java
+++ src/main/java/models/command/RegisterMenuCommand.java
@@ -6,9 +6,9 @@ public enum RegisterMenuCommand implements Command {
     EXIT_MENU("menu exit"),
     ENTER_MENU("\\s*menu\\s*enter\\s*(?<menuName>\\S+)\\s*"),
     SHOW_CURRENT_MENU("\\s*show\\s+current\\s+menu\\s*"),
-    register("\\s*register -u (?<username>\\S+) -p (?<password>\\S+) (?<passwordConfirm>\\S+) -n (?<nickname>\\S+)" +
+    REGISTER("\\s*register -u (?<username>\\S+) -p (?<password>\\S+) (?<passwordConfirm>\\S+) -n (?<nickname>\\S+)" +
             " -e (?<email>\\S+) -g (?<gender>\\S+)\\s*"),
-    pickQuestion("\\s*pick\\s*question\\s*-q\\s*(?<questionNumber>\\d+)\\s*-a\\s*(?<answer>\\S+)\\s*-c\\s*(?<answerConfirm>\\S+)\\s*"),
+    PICK_QUESTION("\\s*pick\\s*question\\s*-q\\s*(?<questionNumber>\\d+)\\s*-a\\s*(?<answer>\\S+)\\s*-c\\s*(?<answerConfirm>\\S+)\\s*"),
     GO_TO_LOGIN("\\s*go\\s*to\\s*login\\s*menu\\s*"),
     ;
     public final Pattern pattern;
