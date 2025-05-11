6e5471db2b24e8dac8ea843c88f20983cffe156f
Alireza <azerila406@gmail.com>
2025-05-11 14:22:14 +0330

    create shop object

--- src/main/java/models/map/Shop.java
+++ src/main/java/models/map/Shop.java
@@ -3,11 +3,13 @@ package models.map;
 import com.google.gson.Gson;
 import controllers.DataBaseController;
 import models.Item.Item;
+import models.data.shop.ShopData;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Shop extends Building {
-	private ArrayList<Item> items = new ArrayList<>();
+	private List<ShopData> items;
 	private final TileType shopType;
 
 	public Shop(TileType shopType) {
@@ -16,18 +18,45 @@ public class Shop extends Building {
 
 		this.shopType = shopType;
 		this.map = (new MapBuilder()).buildShop();
+		items = ShopData.getRecipeByShop(getShopName());
 	}
+
+	public String getShopName() {
+		String name;
+		switch (shopType) {
+			case BLACKSMITH:
+				name = "Blacksmith";
+				break;
+			case JOJAMART:
+				name = "JojaMart";
+				break;
+			case PIERR_STORE:
+				name = "Pierre's General Store";
+				break;
+			case CARPENTER_SHOP:
+				name = "Carpenter's Shop";
+				break;
+			case FISH_SHOP:
+				name = "Fish Shop";
+				break;
+			case MARINE_SHOP:
+				name = "Marnie's Ranch";
+				break;
+			case STARDROP_SALOON:
+				name = "The Stardrop Saloon";
+				break;
+			default:
+				throw new IllegalArgumentException("Unknown shop type: " + shopType);
+		}
+		return name;
+	}
+
 
 	public ArrayList<String> showProducts(){
 		ArrayList<String > output = new ArrayList<>();
 		output.add(shopType.toString() + " :");
 
-		for(Item item : items){
-			output.add("Item: " + item.getName());
-			output.add("price: " + item.getPrice());
-			output.add("amount: " + item.getAmount());
-			output.add("---------------");
-		}
+		//TODO
 		return output;
 	}
 
