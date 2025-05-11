1eee3f8fe2babf58b6e7cac7fd51318d013f2e7d
azerila406 <107407273+azerila406@users.noreply.github.com>
2025-05-11 11:09:54 +0330

    add lakes and const random (#90)

--- src/main/java/models/App.java
+++ src/main/java/models/App.java
@@ -4,12 +4,15 @@ import models.data.DataLoader;
 import models.game.Game;
 import models.user.User;
 
+import java.util.Random;
+
 public class App {
     public static Menu currentMenu = Menu.RegisterMenu;
     public static Game game = null;
     public static boolean play = true;
     public static User registeredUser;
     public static User logedInUser;
+    public static final Random random = new Random(1);
 
     static {
         DataLoader.load();
