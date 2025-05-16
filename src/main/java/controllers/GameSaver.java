7ea9b297a2561b95aea566ae876be77c29436000
Alireza <azerila406@gmail.com>
2025-05-16 15:12:42 +0330

    game save

--- src/main/java/controllers/GameSaver.java
+++ src/main/java/controllers/GameSaver.java
@@ -12,6 +12,7 @@ import java.io.*;
 public class GameSaver {
 
     public static void saveApp(App app, String filename) {
+        filename = "user-data/" + filename + ".dat";
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
             out.writeObject(app);
         } catch (IOException e) {
@@ -20,11 +21,13 @@ public class GameSaver {
     }
 
     public static Result<App> loadApp(String filename) {
+        filename = "user-data/" + filename + ".dat";
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
             App app = (App) in.readObject();
             System.out.println("Game loaded successfully.");
             return Result.success(app);
         } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
             return Result.failure(GameError.GAME_NOT_FOUND);
         }
     }
