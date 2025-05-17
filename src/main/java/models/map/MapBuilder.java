3e36b14861492f35d7fe22875578d640a69cea5c
Alireza <azerila406@gmail.com>
2025-05-17 09:56:18 +0330

    thor cheat tested

--- src/main/java/models/map/MapBuilder.java
+++ src/main/java/models/map/MapBuilder.java
@@ -89,7 +89,7 @@ public class MapBuilder {
                     int id = random.nextInt(0, numberOfForagingTreeTypes); // What is this foraging tree's type?
                     int day = random.nextInt(0, 60); // How old is this foraging tree?
                     PlantedTree plantedTree = (new Sapling(TreeData.getData(id), ItemType.SAPLING, 1)).plant(map.getTile(coord));
-                    map.getTile(coord).setTileType(TileType.TREE);
+                    map.getTile(coord).setTileType(TileType.PLANTED_TREE);
                     plantedTree.setDay(day);
                     numberOfRemainingForagingTrees--;
                 }
