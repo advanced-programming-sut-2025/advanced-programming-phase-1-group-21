870698d79b2a78f220e1a0d0857f716849cc612d
Alireza <azerila406@gmail.com>
2025-08-05 17:25:48 +0330

    API

--- server/src/main/java/router/LLMServiceRouter.java
+++ server/src/main/java/router/LLMServiceRouter.java
@@ -1,4 +1,35 @@
 package router;
 
-public class LLMServiceRouter {
+import models.network.Message;
+import com.esotericsoftware.kryonet.Connection;
+import network.ServiceRouter;
+import services.LLMService;
+import services.LobbyService;
+import session.SessionManager;
+import util.NetworkUtil;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LLMServiceRouter implements ServiceRouter {
+    private static final Map<String, Method> methods = new HashMap<>();
+
+    static {
+        for (Method method : LLMService.class.getDeclaredMethods()) {
+            methods.put(method.getName(), method);
+        }
+    }
+
+    @Override
+    public Object dispatch(Connection conn, Message msg) throws Exception {
+        String methodName = msg.getMethodName();
+        Map<String, Object> args = (Map<String, Object>) msg.data;
+        Method method = methods.get(methodName);
+        if (method == null) {
+            throw new RuntimeException("[DISPATCH ERROR] Method not found: " + methodName);
+        }
+        Object[] parsedArgs = NetworkUtil.mapToArgs(method, args);
+        return method.invoke(SessionManager.getLobbyService(conn), parsedArgs);
+    }
 }
--- server/src/main/java/services/LLMService.java
+++ server/src/main/java/services/LLMService.java
@@ -1,4 +1,15 @@
 package services;
 
+import com.esotericsoftware.kryonet.Connection;
+
 public class LLMService {
+
+    public final Connection connection;
+
+    public LLMService(Connection conn) {
+        connection = conn;
+    }
+
+
+
 }
